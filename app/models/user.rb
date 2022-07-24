bd06f568fe9b23beaa63d65bc7ab6037080cf478
Freigth <cosiconeal@gmail.com>
2022-07-24 15:29:50 +0800

    fix sign_up bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
-  # :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
+  # :recoverable, :rememberable, 
 
   validates :email, presence: true, length:{ minimum: 2 }
   validates :password, presence: true, length:{ minimum: 6 }
