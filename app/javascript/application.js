7ce79494175568e6a49937c9815cad68e353ab91
Freigth <cosiconeal@gmail.com>
2022-07-26 17:41:31 +0800

    add error validation on signup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
